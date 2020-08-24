a0d494b8512edb7e38c2ef4a6a0279a3dd3fbfb6
Grant <grant.maki@integral.io>
2020-08-24 10:08:10 -0400

    added preferences page

--- background-script.js
+++ background-script.js
@@ -1,14 +1,15 @@
 var zoomSuccessRegex = /^((http[s]?):\/)?\/?((\/\w+)*)(.*)(zoom.us)((\/\w+)*\/)(.*)?(status=|#)success$/
 var webexRegex = /^.*\.webex\.com.*\/meeting\/.*$/
 
+let delayInMinutes = 0.1
 
-function closeZoomTab(tabId, changeInfo, tab) {
+function closeMeetingTab(tabId, changeInfo, tab) {
     if (changeInfo.url) {
         if (zoomSuccessRegex.test(changeInfo.url)) {
-			browser.alarms.create("killZoom" + tabId, {delayInMinutes:0.1});
+			browser.alarms.create("killZoom" + tabId, {delayInMinutes:delayInMinutes});
         }
         if (webexRegex.test(changeInfo.url)) {
-			browser.alarms.create("killZoom" + tabId, {delayInMinutes:0.1});
+			browser.alarms.create("killZoom" + tabId, {delayInMinutes:delayInMinutes});
         }
     }
 }
@@ -17,5 +18,20 @@ function handleAlarm(alarmInfo){
 	const tabId = parseInt(alarmInfo.name.slice(8))
 	browser.tabs.remove(tabId);
 }
-browser.tabs.onUpdated.addListener(closeZoomTab);
+browser.tabs.onUpdated.addListener(closeMeetingTab);
 browser.alarms.onAlarm.addListener(handleAlarm);
+
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+  if (item.seconds) {
+    let seconds = parseInt(item.seconds);
+	delayInMinutes = seconds / 60;
+  }
+}
+
+let getting = browser.storage.sync.get("seconds");
+getting.then(onGot, onError);
